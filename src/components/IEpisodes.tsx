0aa5e0694372f4b05228c9f847e3e9906f52795f
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-11-08 15:39:47 +0000

    Add types to IEpisodes components

---
+++ src/components/IEpisodes.tsx
@@ -0,0 +1,27 @@
+interface IEpisode {
+    id: number;
+    url: string;
+    name: string;
+    season: number;
+    number: number;
+    type: string;
+    airdate: string;
+    airtime: string;
+    airstamp: string;
+    runtime: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    _links: { self: { href: string } };
+};
+
+function IEpisodes(episodes: IEpisode): JSX.Element {
+    return (
+        <>
+        </>
+    );
+};
+
+export default IEpisodes;
