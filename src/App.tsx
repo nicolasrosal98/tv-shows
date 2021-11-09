fdf178027a6c05d9a6451ace4ef1398b190adafd
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-11-09 11:31:15 +0000

    Use Episode prop in MainBody prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./websitestyle.css";
-// import episodes from "./episodes.json";
 import Footer from "./components/Footer";
 import MainBody from "./components/MainBody";
 import NavigationBar from "./components/NavigationBar";
