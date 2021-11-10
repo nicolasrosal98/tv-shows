d4499a82c199b18d6074e0ff1a85881e410785df
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-11-10 09:54:34 +0000

    Add style and logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,16 @@
 function Footer(): JSX.Element {
   return (
-    <>
+    <div className="footer">
+      <img
+        src="https://i.ibb.co/m04dsj8/Free-Sample-By-Wix.jpg"
+        alt="Free-Sample-By-Wix"
+        className="logo"
+      />
       <p>
         The <a href="https://api.tvmaze.com/shows/82/episodes">data</a> was
         collected using TVMaze's API
       </p>
-    </>
+    </div>
   );
 }
 
