da2169413cda1ba845708514fcb67218c41ee716
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-11-10 12:16:50 +0000

    Add search bar and dropdown select to body

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -4,7 +4,20 @@ import episodes from "../episodes.json";
 function MainBody(): JSX.Element {
   return (
     <div className="body-div">
-      <h2>Episodes</h2>
+      <nav>
+        <h2>Episodes</h2>
+        <form>
+          <select name="pets" id="pet-select">
+            <option value="">Select an episode</option>
+            <option value="dog">Option1</option>
+            <option value="cat">Option2</option>
+          </select>
+          <>
+            <input type="text" placeholder="Search.."></input>
+            <p>Displaying 73/73 episodes</p>
+          </>
+        </form>
+      </nav>
       <div className="grid">
         <>
           {episodes.map((episode) => (
