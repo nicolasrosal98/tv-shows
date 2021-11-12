21155e3bd1c47fb58f480c8cb09411c55b79bbd2
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-11-12 11:22:12 +0000

    Reset search bar after dropdown selection

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -4,53 +4,104 @@ import episodes from "../episodes.json";
 
 function MainBody(): JSX.Element {
   const [searchValue, setSearchValue] = useState<string>("");
-  const selectedEpisodes = episodes.filter(
+  const [dropDownValue, setDropDownValue] = useState<string>("");
+
+  const selectedEpisodesDropDown = episodes.filter(
+    (episode) =>
+      episode.summary.includes(dropDownValue) ||
+      episode.name.includes(dropDownValue)
+  );
+
+  const selectedEpisodesSearch = episodes.filter(
     (episode) =>
       episode.summary.includes(searchValue) ||
       episode.name.includes(searchValue)
   );
+  const selectedDropdown = episodes.map((episode) =>
+    episode.number <= 9
+      ? `S0${episode.season}E0${episode.number} - ${episode.name}`
+      : `S0${episode.season}E${episode.number} - ${episode.name}`
+  );
+
+  // const resetDropDown = () => {
+  //   document.getElementById("episode-select").selectedValue = "";
+  // };
 
   return (
     <div className="body-div">
       <nav>
         <h2>Episodes</h2>
         <form>
-          <select name="pets" id="pet-select">
+          <select
+            name="episode"
+            id="episode-select"
+            onChange={(event) => {
+              setDropDownValue(event.target.value);
+              setSearchValue("");
+            }}
+          >
             <option value="">Select an episode</option>
-            <option value="dog">Option1</option>
-            <option value="cat">Option2</option>
+            {selectedDropdown.map((episodeName) => (
+              <option
+                value={episodeName.split("- ")[1]}
+                key={selectedDropdown.indexOf(episodeName)}
+              >
+                {episodeName}
+              </option>
+            ))}
           </select>
           <>
             <input
               value={searchValue}
               onChange={(event) => {
                 setSearchValue(event.target.value);
+                setDropDownValue("");
+                // resetDropDown;
               }}
             />
-            <p>Displaying {selectedEpisodes.length}/73 episodes</p>
+            <p>Displaying {selectedEpisodesSearch.length}/73 episodes</p>
           </>
         </form>
       </nav>
       <div className="grid">
         <>
-          {selectedEpisodes.map((episode) => (
-            <Episode
-              id={episode.id}
-              url={episode.url}
-              name={episode.name}
-              season={episode.season}
-              number={episode.number}
-              type={episode.type}
-              airdate={episode.airdate}
-              airtime={episode.airtime}
-              airstamp={episode.airstamp}
-              runtime={episode.runtime}
-              image={episode.image}
-              summary={episode.summary}
-              _links={episode._links}
-              key={episode.id}
-            />
-          ))}
+          {searchValue
+            ? selectedEpisodesSearch.map((episode) => (
+                <Episode
+                  id={episode.id}
+                  url={episode.url}
+                  name={episode.name}
+                  season={episode.season}
+                  number={episode.number}
+                  type={episode.type}
+                  airdate={episode.airdate}
+                  airtime={episode.airtime}
+                  airstamp={episode.airstamp}
+                  runtime={episode.runtime}
+                  image={episode.image}
+                  summary={episode.summary}
+                  _links={episode._links}
+                  key={episode.id}
+                />
+              ))
+            : selectedEpisodesDropDown.map((episode) => (
+                <Episode
+                  id={episode.id}
+                  url={episode.url}
+                  name={episode.name}
+                  season={episode.season}
+                  number={episode.number}
+                  type={episode.type}
+                  airdate={episode.airdate}
+                  airtime={episode.airtime}
+                  airstamp={episode.airstamp}
+                  runtime={episode.runtime}
+                  image={episode.image}
+                  summary={episode.summary}
+                  _links={episode._links}
+                  key={episode.id}
+                />
+              ))}
         </>
       </div>
     </div>
